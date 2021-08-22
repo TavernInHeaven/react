1adea6eb6203629e19094644e54bb21e26114df1
TavernInHeaven <nty2013@yahoo.com>
2021-08-22 21:32:07 +1000

    show no task if tasks list is empty

--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -1,6 +1,7 @@
 import Task from "./Task"
 
 const Tasks = ({tasks, onDelete}) => {//rac
+    if(tasks.length === 0) return <h3>Hooray! No task here</h3>
     return (
         <ul>{
                 tasks.map((t)=> (<Task key={t.id} task={t} onDelete={onDelete}/>)            
